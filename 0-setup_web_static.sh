5c4491d191530624619147676c42dc50cd9cf2e1
forgetmudyanadzo <forgetmudyanadzo87@gmail.com>
2023-10-08 08:39:39 -0700

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,35 @@
 #!/usr/bin/env bash
 # Set up web server
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
-echo "Hello world" | sudo tee /data/web_static/releases/test/index.html
-CURRENT="/data/web_static/current"
-if [ -L "$CURRENT" ]; then
-    sudo rm "$CURRENT"
-fi
-sudo ln -s /data/web_static/releases/test/ "$CURRENT"
-sudo chown -hR ubuntu:ubuntu /data
-sudo sed -i "40i location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
-sudo service nginx restart
+apt-get update
+apt-get install -y nginx
+
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $hostname;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://github.com/forgetmudyanadzo;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
